8b52f4eb626569bf0e6887a918ecae76b250ae7d
Uka Emmanuel Ogbonna <ukaemmanuel200@gmail.com>
2021-11-10 12:55:09 +0100

    updates

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,10 +1,24 @@
 import React from "react"
+import './Login.css'
 
 const Login = () => {
   return (
     <div>
-      <h3 className="loginPage">You are not loggedIn</h3>
+      <div className="login">
+      <img 
+        className='linkedinImage' 
+        src="https://news.hitb.org/sites/default/files/styles/large/public/field/image/500px-LinkedIn_Logo.svg__1.png" 
+          alt="Linkedin"  
+        />
+        <form action="" >
+          <input  
+            type="text"
+            placeholder="Full name required if registering"
+          />
+        </form>
     </div>
+    </div>
+    
   )
 }
 

58ceb505e3928f989d8e27b59fbc212dea025385
Uka Emmanuel Ogbonna <ukaemmanuel200@gmail.com>
2021-11-10 17:48:39 +0100

    updates

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,25 +1,35 @@
 import React from "react"
 import './Login.css'
-
 const Login = () => {
+  const register = () => {
+
+  };
+  const login = () => {
+
+  };
   return (
     <div>
       <div className="login">
       <img 
         className='linkedinImage' 
         src="https://news.hitb.org/sites/default/files/styles/large/public/field/image/500px-LinkedIn_Logo.svg__1.png" 
-          alt="Linkedin"  
+          alt="Linkedin"
         />
         <form action="" >
           <input  
             type="text"
             placeholder="Full name required if registering"
           />
+          <input placeholder="Profile pic URL (optional)" type="text" />
+          <input placeholder="Email" type="email" />
+          <input placeholder="password" type="password" />
+          <button onClick={login}>Sign In</button>
+          <p className>Not a member? {" "}
+            <span className="login_registering" onClick={register}>Register Now</span>
+          </p>
         </form>
     </div>
     </div>
-    
   )
 }
-
 export default Login
